70a8ce877e4b12007f04acb174632ed1d961ed40
Jéssica Motta <jessicalimamotta@gmail.com>
2025-01-25 13:21:02 -0300

    adjust header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,8 +1,16 @@
+import EduIcon from '/app/images/edu-icon.svg';
+import { IoMenu } from "react-icons/io5";
+import Image from 'next/image';
 const Header = () => {
     return(
         <div className={`bg-green-10 max-w-full h-[50px] flex items-center justify-between p-6 text-white`}>
-        <p>LOGO</p>
-        <p>MENU</p>
+        <button>
+            <Image 
+            src={EduIcon}
+            alt='EduIcon'
+            width={32} />
+        </button>
+        <button><IoMenu className='w-8'/></button>
         </div>
     )
 }
