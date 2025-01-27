7eee229b2a271ebb33126e6976ea7df0ab6b3817
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-01-27 15:25:53 -0300

    fix: login btn to student page

--- components/page-templates/login/index.tsx
+++ components/page-templates/login/index.tsx
@@ -10,7 +10,10 @@ export const Login = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-white px-4 md:px-16">
       <div className="absolute top-6 left-10 flex items-center gap-3 sm:w-1/5">
         <Image src={logo} alt="Company Logo" width={68} height={68} />
-        <h1 className="text-lg font-bold" style={{ color: "var(--color-green-9)" }}>
+        <h1
+          className="text-lg font-bold"
+          style={{ color: "var(--color-green-9)" }}
+        >
           Level UP Academy
         </h1>
       </div>
@@ -37,21 +40,33 @@ export const Login = () => {
             </div>
             <div className="flex items-center">
               <input type="checkbox" className="h-4 w-4 text-blue-500" />
-              <span className="ml-2 text-gray-600 text-sm">Lembrar do meu login!</span>
+              <span className="ml-2 text-gray-600 text-sm">
+                Lembrar do meu login!
+              </span>
             </div>
           </form>
           <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition">
-            <a href="/home">Entrar</a>
+            <a href="/aluno">Entrar</a>
           </button>
         </div>
 
         <div className="hidden md:block w-full md:w-1/2 relative">
           <div className="relative w-full ">
-            <Image src={loginImage} alt="Study" layout="full" objectFit="cover" className="" />
+            <Image
+              src={loginImage}
+              alt="Study"
+              layout="full"
+              objectFit="cover"
+              className=""
+            />
             <div className="absolute inset-0 flex flex-col justify-end p-8 text-white">
-              <h2 className="text-2xl font-bold">Estude, evolua e conquiste!</h2>
+              <h2 className="text-2xl font-bold">
+                Estude, evolua e conquiste!
+              </h2>
               <p className="mt-2 text-sm w-4/5 text-slate-200">
-                A cada tarefa concluída, novas recompensas são desbloqueadas, tornando o processo de aprendizagem mais interativo e recompensador.
+                A cada tarefa concluída, novas recompensas são desbloqueadas,
+                tornando o processo de aprendizagem mais interativo e
+                recompensador.
               </p>
             </div>
           </div>
