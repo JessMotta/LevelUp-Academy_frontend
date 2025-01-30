afc2c0537b8229a9d03e879364c15d28ef354c26
Jéssica Motta <jessicalimamotta@gmail.com>
2025-01-30 20:45:20 -0300

    fix card user

--- components/ui/CardUser/index.tsx
+++ components/ui/CardUser/index.tsx
@@ -18,30 +18,45 @@ const student = {
 // }
 
 const CardUser = () => {
-    return (
-        <div className={`
-            bg-primary rounded-[12px] w-[300px] h-auto mx-auto my-4
-            `}>
-            <div className="text-white p-4 flex items-center justify-start">
-                <Image
-                className='mr-4'
-                src={ProfileImage}
-                alt='Foto de perfil'
-                width={54}
-                height={54}
-                />
-                <div className='mr-4'>
-                    <h3 className="text-base font-medium">{student.name}</h3>
-                    <p className="text-[10px] font-medium">{student.classroom}</p>
-                    <div>
-                        <div className="flex items-center mt-2">
-                            <IoLogoElectron className='mr-2 fill-green-12'/>
-                            <div className="uppercase text-[10px] font-bold">{student.patent}</div>
-                        </div>
-                        <ProgressBar percent={student.progress}/>
-                    </div>
-                </div>
+  const { user: student } = useSessionContext();
+
+  if (!student) return <></>;
+
+  return (
+    <div className="w-full mx-auto md:w-3/4">
+    <div
+      className={`
+            bg-primary rounded-[12px] h-auto m-3
+            `}
+    >
+      <div className="text-white p-3 flex items-center justify-start">
+        <Image
+          className="mr-4"
+          src={ProfileImage}
+          alt="Foto de perfil"
+          width={54}
+          height={54}
+        />
+        <div className="mr-4">
+          <h3 className="text-base font-medium">{student.name}</h3>
+          <p className="text-[10px] font-medium">{student.schollYear}</p>
+          <div>
+            <div className="flex items-center mt-2">
+              <IoLogoElectron className="mr-2 fill-green-12" />
+              <div className="uppercase text-[10px] font-bold">
+                {student.patent}
+              </div>
             </div>
+            <ProgressBar
+              percent={student.currentExperience / student.nextPatentExperience}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
 
         </div>
     )
