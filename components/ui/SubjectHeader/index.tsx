696ee0f2079ab177b7977531d5f254086cd92aa6
thegaab <tiagogabrielcontato@hotmail.com>
2025-01-30 16:05:02 -0300

    Update: icone de prestigio + correção da cor

--- components/ui/SubjectHeader/index.tsx
+++ components/ui/SubjectHeader/index.tsx
@@ -2,6 +2,9 @@ import { ReactNode } from "react";
 import { useSubjectContext } from "@/providers/SubjectProvider";
 import { useRouter } from "next/navigation";
 
+import Image from "next/image";
+import star from "@/public/assets/star.svg";
+
 const Box = ({ children }: { children: ReactNode }) => {
   return (
     <div className="rounded-lg bg-brand-800 text-white py-2.5 px-3.5">
@@ -40,11 +43,18 @@ export default function SubjectHeader() {
           <p>Prof. {teacher}</p>
         </div>
         <button
-          className="rounded-full bg-brand-900 p-1 w-16 h-16 flex justify-center items-center"
+          className="relative rounded-full bg-brand-900 p-1 w-16 h-16 flex justify-center items-center"
+          style={{ color: "var(--color-yellow)" }}
           onClick={() =>
             router.push(`/aluno/disciplina/${subjectId}/beneficios`)
           }
         >
+        <Image
+            src={star}
+            alt="Star"
+            width={30}
+            className="absolute left-[-4px] top-[-4px] "
+          />
           {pointsAmount}
         </button>
       </div>
