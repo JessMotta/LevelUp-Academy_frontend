5fb5f2e6db2e021413aa8a55096b355a8bfcf269
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-01-30 16:14:08 -0300

    feat: request hook + subjects list request

--- components/ui/SubjectList/index.tsx
+++ components/ui/SubjectList/index.tsx
@@ -2,27 +2,28 @@
 import { SubjectReport } from "@/types/types";
 import { useEffect, useState } from "react";
 import SubjectCard from "../SubjectCard";
-import { SUBJECTS_MOCK } from "@/__mocks__/subjects";
+import useStudentSubjects from "@/api/requests/studentSubjects";
 
 export function SubjectList() {
   const [subjects, setSubjects] = useState<SubjectReport[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+  const reqSubjects = useStudentSubjects();
 
   useEffect(() => {
-    /**
-     * Aqui deveria fazer uma chamada de consulta pra pegar esses dados do aluno
-     */
-    setLoading(true);
-    setTimeout(() => {
-      setSubjects(SUBJECTS_MOCK);
-      setLoading(false);
-    }, 1500);
+    fetchSubjects();
   }, []);
+
+  async function fetchSubjects() {
+    const data = await reqSubjects.submit();
+    setSubjects(data);
+  }
 
   return (
     <div className="w-full flex flex-col items-center justify-center gap-2 px-4">
-      {loading && <>Loading...</>}
-      {!loading &&
+      {reqSubjects.loading && <>Loading...</>}
+      {!reqSubjects.loading && subjects.length <= 0 && (
+        <>Sem dados no momento</>
+      )}
+      {!reqSubjects.loading &&
         subjects.map((s) => <SubjectCard key={s.id} studentReport={s} />)}
     </div>
   );
