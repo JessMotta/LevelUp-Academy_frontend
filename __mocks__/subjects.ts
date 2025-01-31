4cf65c1d996baa3bfce5fa08940dc79667bec961
Carolina Moutella <emaildacarolmoutella@gmail.com>
2025-01-31 19:55:08 -0300

    corrige tipagem dos ids

--- __mocks__/subjects.ts
+++ __mocks__/subjects.ts
@@ -2,21 +2,21 @@ import { SubjectReport } from "@/types/types";
 
 export const SUBJECTS_MOCK: SubjectReport[] = [
   {
-    id: "1",
+    id: 1,
     subject: "Matemática",
     teacher: "Ana Cristina",
     attendency: 84.2,
     average: 77,
   },
   {
-    id: "2",
+    id: 2,
     subject: "Português",
     teacher: "Veronica Leão",
     attendency: 91,
     average: 86,
   },
   {
-    id: "3",
+    id: 3,
     subject: "Ciências",
     teacher: "Paulo Gustavo",
     attendency: 65,
