8afe82a2f7a4c1897e303832f042461b016104e5
lucasmoraist <luksmnt1101@gmail.com>
2025-02-01 11:53:49 -0300

    feat: enhance ItemStore component with props and update styles

--- components/ui/ItemStore/index.tsx
+++ components/ui/ItemStore/index.tsx
@@ -1,20 +1,27 @@
 import Image from 'next/image';
 import StarIcon from '/public/assets/star.svg';
 
-const ItemStore = () => {
-  const data = {
-    title: 'Apagar uma falta',
-    description: 'Faltou algum dia de aula? Anule essa falta',
-    price: 40,
-  };
+interface ItemStoreProps {
+  title: string;
+  description: string;
+  price: number;
+}
+
+interface Props {
+  item?: ItemStoreProps;
+  onCLick?: () => void;
+}
+
+const ItemStore = ({ item, onCLick }: Props) => {
+  if (!item) return null;
 
   return (
-    <div className="bg-brand-600 w-[144px] h-auto rounded-[12px] shadow-[0px_4px_4px_rgba(138,138,138,0.25)] mx-auto my-4">
+    <div className="bg-brand-600 min-w-36 max-w-40 h-auto rounded-md shadow-[0px_4px_4px_rgba(138,138,138,0.25)] mx-auto my-4">
       <div className="text-white p-4 flex flex-col items-center justify-start gap-2">
         <div className="flex flex-col gap-2 items-center">
-          <h4 className="font-semibold text-base">{data.title}</h4>
-          <p className="text-center w-[133px] text-[10px]">
-            {data.description}
+          <h4 className="font-semibold text-base">{item.title}</h4>
+          <p className="text-center min-w-36 max-w-40 text-xxs">
+            {item.description}
           </p>
           <div className="flex flex-row gap-2 items-center">
             <Image
@@ -23,10 +30,13 @@ const ItemStore = () => {
               width={16}
               height={16}
             />
-            <span className="text-medium text-sm">{data.price}</span>
+            <span className="text-medium text-sm">{item.price}</span>
           </div>
         </div>
-        <button className="border rounded-[20px] w-[67px] h-[22px] text-[10px] text-medium">
+        <button
+          className="border rounded-md min-w-16 max-w-20 min-h-5 max-h-6 text-[10px] text-medium"
+          onClick={onCLick}
+        >
           Adquirir
         </button>
       </div>
