bf0a26a370e1499d96e206332df8e083b6d2f25e
Jéssica Motta <jessicalimamotta@gmail.com>
2025-02-05 21:39:17 -0300

    get data from mock

--- components/page-templates/student/prestige/owned/index.tsx
+++ components/page-templates/student/prestige/owned/index.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { STUDENT_PRESTIGE_MOCK } from "@/__mocks__/subjectMainData";
+import { useSubjectContext } from "@/providers/SubjectProvider"
+
 export default function OwnedBenefitsTemplate() {
-  return <div className="flex flex-col text-white">BENEFICIOS DISPONIVEIS</div>;
+  //const {subjectData} = useSubjectContext();
+  const prestige = STUDENT_PRESTIGE_MOCK;
+
+  return (
+    <div className="w-full flex flex-col items-center justify-center gap-2 px-4">
+    {!prestige.ownedBenefits ||
+      (prestige.ownedBenefits.length <= 0 && (
+        <div>Não foi possível encontrar suas matérias no momento.</div>
+      ))}
+    {prestige.ownedBenefits.length >= 1 &&
+      prestige.ownedBenefits.map((s) => (
+         <div key={s.id}>{s.title}</div>
+        // <SubjectCard key={s.id} subjectData={s} subjectId={s.id.toString()} />
+      ))}
+  </div>
+  ) 
 }
